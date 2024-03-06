92c44b4dfc3cf39d065451e9d3ce218bacfa8509
christiansaccani <christian.saccani99@gmail.com>
2024-03-06 19:43:12 +0100

    1/2 Milestone 2 done

--- src/components/AppCardItem.vue
+++ src/components/AppCardItem.vue
@@ -12,8 +12,12 @@ export default {
     <li>
         <p class="card-title">{{ film.title }}</p>
         <section>
-            <p class="card-otitle">{{ film.original_title }}</p>
             <p class="card-lang">{{ film.original_language }}</p>
+            <img :src="'https://flagcdn.com/w20/' + (film.original_language === 'en' ? 'us'
+            : film.original_language === 'ko' ? 'kr'
+            : film.original_language === 'ja' ? 'jp'
+            : film.original_language) + '.png'">
+            <p class="card-otitle">{{ film.original_title }}</p>
             <p class="card-vote">{{ (film.vote_average).toFixed(2) }}</p>
         </section>
     </li>
@@ -33,6 +37,10 @@ li {
 
         padding-bottom: .50em;
         border-bottom: 1px solid $primaryColor;
+
+        white-space: nowrap;
+        overflow: hidden; // Nasconde il testo che supera le dimensioni disponibili
+        text-overflow: ellipsis;
     }
 
     section {
