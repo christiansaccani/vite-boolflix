452fcf9b34c8f30bb1445351ffaf9ca3d8342e8c
christiansaccani <christian.saccani99@gmail.com>
2024-03-06 14:50:23 +0100

    axios link done and functioning (no key present in commit)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,39 @@
-<script setup>
+<script>
+
+import axios from 'axios';
+
+import {store} from './store.js';
+
+export default {
+  data() {
+    return {
+      films: [],
+      store,
+    }
+  },
+
+  components: {
+
+  },
+
+  created() {
+    axios
+    .get('https://api.themoviedb.org/3/search/movie?&query=maze+runner') //tolgo momentaneamente la key protetta
+    .then(res => {
+      const films = res.data.results;
+      films.forEach(film => {
+        store.films.push(film);
+      });
+    console.log(store.films);
+    });
+
+  },
+
+  methods: {
+
+
+  },
+}
 
 </script>
 
